56dc80d59f2e6b7b370b0d15a9b1eb329a5f6889
fhk2022 <faisal.fhk1992@gmail.com>
2022-12-23 20:49:07 +0530

    feaure1

--- src/helloworld/mainapp/Demo.java
+++ src/helloworld/mainapp/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		String s="faisal";
 		System.out.println("Hello World check merge conflicts");
 		System.out.println("Doing changes to resolve megre conflicts");
 
