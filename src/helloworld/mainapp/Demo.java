17439f0ad633d18aa3781015ab12f8efe30362fb
fhk2022 <faisal.fhk1992@gmail.com>
2022-12-23 20:41:20 +0530

    Commitning in feature branch 2

--- src/helloworld/mainapp/Demo.java
+++ src/helloworld/mainapp/Demo.java
@@ -5,8 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World");
-		
+		System.out.println("Hello World check merge conflicts");
+		System.out.println("Doing changes to resolve megre conflicts");
 
 	}
 
